56b4bef8dcff097a47ae0d205991e341a2a40822
Lekan Adetunmbi <lothbroch@gmail.com>
2021-09-08 08:32:01 +0100

    Server rewritten with Express JS

--- README.md
+++ README.md
@@ -1,16 +1,20 @@
-# A Basic Node.js Website
+# A Basic NodeJS Website
 
 ### About
 
 Basic Informational Site from [the Odin Project](https://www.theodinproject.com/paths/full-stack-javascript/courses/nodejs/lessons/basic-informational-site) Curriculum.
 
-This is just a test project to understand Node.js basics
+A test project to learn and understand ExpressJS basics
 
 ### Contributors
 
 * Lekan Adetunmbi
 
+### Tools
+
+* [ExpressJS](https://expressjs.com)
+
 ### Info
 
 * License: MIT
-* [See it live here:](https://pollaroid.net/basic-informational-site)
+* [See it live here:](https://basic-informational-site.greazleay.repl.co/)
--- index.js
+++ index.js
@@ -1,53 +1,24 @@
-const http = require("http");
+const express = require('express')
 const path = require("path");
-const fs = require("fs");
 
-const server = http.createServer((req, res) => {
+const app = express();
 
-    let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url)
-    let extname = path.extname(filePath);
-    let contentType = 'text/html';
+app.listen(8080);
 
-    switch (extname) {
-        case '.js':
-            contentType = 'text/javascript'
-            break;
-        case '.css':
-            contentType = 'text/css'
-            break;
-        case '.json':
-            contentType = 'application/json'
-            break;
-        case '.png':
-            contentType = 'image/png'
-            break;
-        case '.jpg':
-            contentType = 'image/jpg'
-            break;
-    }
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: path.join(__dirname, 'public')})
+});
 
-    fs.readFile(filePath, (err, content) => {
-        if (err) {
-            if (err.code === 'ENOENT') {
-                // Page not found
-                fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content) => {
-                    res.writeHead(200, { 'Content-Type': 'text/html' })
-                    res.end(content, 'utf8')
-                })
-            } else {
-                // Some server error
-                res.writeHead(500);
-                res.end(`Server Error: ${err.code}`)
-            }
-        } else {
-            // Success
-            res.writeHead(200, { 'Content-Type': contentType });
-            res.end(content, 'utf8');
-        }
-    })
+app.get('/about', (req, res) => {
+    res.sendFile('about.html', { root: path.join(__dirname, 'public')})
+});
 
+app.get('/contact', (req, res) => {
+    res.sendFile('contact.html', { root: path.join(__dirname, 'public')})
+});
+
+app.use(express.static('public'))
+
+app.use((req, res) => {
+    res.status(404).sendFile('404.html', { root: path.join(__dirname, 'public') })
 })
-
-const PORT = process.env.PORT || 8080;
-
-server.listen(PORT)
